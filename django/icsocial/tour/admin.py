cd99b61363de1b9a4cdead05abb9e4be20098ac3
Maria <mariakouppari@gmail.com>
2012-12-01 12:08:54 +0000

    location added to the tour app and added to admin

--- django/icsocial/tour/admin.py
+++ django/icsocial/tour/admin.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.contrib import admin
+from tour.models import *
+
+admin.site.register(Location)
+admin.site.register(Tour)
+admin.site.register(Comment)
+admin.site.register(SubComment)
+
