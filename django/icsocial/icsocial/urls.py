4fb57a9ea7ca97016a4dcccc9aa0ae79e5b286dd
Yiannis <gianniscy@gmail.com>
2012-12-01 20:01:04 +0000

    merging

--- django/icsocial/icsocial/urls.py
+++ django/icsocial/icsocial/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls import patterns, include, url
-from views import login,place,profile,index,dummy
+from views import login,place,profile,index,logout
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', index),
     url(r'^login/', login),
+    url(r'^logout/', logout),
+
 
     url(r'^places/', index),
 
     url(r'^tours/', index),
     url(r'^tours/my/', index),
-    
+
     url(r'^at/(?P<nick>\w{1,50})/$', place),
     url(r'^hello/(?P<nick>\w{1,50})/$', profile),
 
--- django/icsocial/icsocial/views.py
+++ django/icsocial/icsocial/views.py
@@ -1,28 +1,52 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login as user_login, logout as user_logout
+
+
+
 
 def nav(flag):
 	#if flag == True then logged in else guest
-	n = {'home': "/" ,'tour': {'buildings':'/places/','tours':'/tours/'},\
-			'about':{'social_ic':'/about/icsocial/','us':'/about/us/',\
-			'tech':'/about/tech/'},'campus':'/campus/'}
-
-	n['tour']['my'] = '/my/tours/' 
+	n = {'home':'home/','tour': {'buildings':'/places/','tours':'/tours/'},\
+	 	'about':{'social_ic':'/about/icsocial/','us':'/about/us/',\
+	 	'tech':'/about/tech/'},'campus':'/campus/','logout':'/logout'}
 	return n
 
 def index(request):
-	navi = nav(False)
-	c = {'nav': nav}
+	logged = request.user.is_authenticated()
+	navi = nav(logged)
+	c = {'nav': navi,'logged': logged}
+	if logged:
+		c['username'] = request.user.username
+	c.update(csrf(request))
 	return render_to_response("index.html",c)
 
 def login(request):
-	return HttpResponse("login")
+	if request.user.is_authenticated():
+		return HttpResponseRedirect("/")
+	if request.method != 'POST':
+		return HttpResponse("login")
+	username = request.POST['username']
+	password = request.POST['password']
+	user = authenticate(username=username, password=password)
+	if user is not None:
+		if user.is_active:
+			user_login(request, user)
+			return HttpResponseRedirect("/")
+		else:
+			return HttpResponse("login: account disabled")
+	else:
+		return HttpResponse("wrong username/password")
+
+def logout(request):
+	if request.user.is_authenticated():
+		HttpResponseRedirect("/")
+	user_logout(request)
+	return HttpResponseRedirect("/")
 
 def place(request,nick):
     return HttpResponse("location:" + nick)
 
 def profile(request,nick):
   return HttpResponse("profile:" + nick)
-
-def dummy(request,title):
-  return HttpResponse("title:" + title)
